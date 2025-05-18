f0c5a790df3b177c8aa9e5b43df2f8be78439619
tijnsemmekrot <tsemmekrot@gmail.com>
2025-05-18 14:01:24 +0200

    testing to see if this works

--- backend/test.py
+++ backend/test.py
@@ -1,26 +1,22 @@
 import os
 from supabase import create_client, Client
-import supabase
 from fastapi import FastAPI
+from dotenv import load_dotenv
 
 app = FastAPI()
+load_dotenv()
 
-
-def initDB():
-    url: str = "https://ucexhnksccudzjcikyzi.supabase.co"
-    key: str = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVjZXhobmtzY2N1ZHpqY2lreXppIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDc0Nzg1NTQsImV4cCI6MjA2MzA1NDU1NH0.ys7zSEM3o0XkrOZbUUPA5k-EcatOrHxsIO36G3Z938M"
-    supabase: Client = create_client(url, key)
-    return supabase
+SUPABASE_URL = os.getenv("SUPABASE_URL")
+SUPABASE_KEY = os.getenv("SUPABASE_KEY")
+supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
 
 
 def addNametoDB():
-    supabase = initDB()
     supabase.table("names").insert({"name": "Julia-test"}).execute()
 
 
 @app.get("/")
 def main():
-    supabase = initDB()
     addNametoDB()
 
     response = supabase.table("names").select("*").execute()
