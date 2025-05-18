c4b7ff031fabe14531b70ed8340af7ed943f728d
tijnsemmekrot <tsemmekrot@gmail.com>
2025-05-18 14:32:26 +0200

    test

--- backend/test.py
+++ backend/test.py
@@ -1,15 +1,43 @@
 import os
 from supabase import create_client, Client
 from fastapi import FastAPI
+from pydantic import BaseModel
 from dotenv import load_dotenv
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 load_dotenv()
 
+origins = [
+    "https://full-stack-test-chi.vercel.app",
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
 SUPABASE_URL = os.getenv("SUPABASE_URL")
 SUPABASE_KEY = os.getenv("SUPABASE_KEY")
-print(SUPABASE_URL, SUPABASE_KEY)
-supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
+
+if not SUPABASE_URL or not SUPABASE_KEY:
+    raise ValueError("Missing SUPABASE_URL or SUPABASE_KEY in environment variables")
+
+supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
+
+
+class NamePayLoad(BaseModel):
+    name: str
+
+
+@app.post("/api/firstName")
+async def fetch_request(payload: NamePayLoad):
+    print("Received name:", payload.name)
 
 
 def addNametoDB():
@@ -18,8 +46,6 @@ def addNametoDB():
 
 @app.get("/")
 def main():
-    addNametoDB()
-
     response = supabase.table("names").select("*").execute()
     print(response.data)
 
--- frontend/js/main.js
+++ frontend/js/main.js
@@ -2,7 +2,7 @@ document.getElementById('nameForm').addEventListener('submit', async (e) => {
   e.preventDefault();
 
   try {
-    const response = await fetch('https://full-stack-test-nddw.onrender.com/api/firstName', {
+    const response = await fetch('https://full-stack-test-28fq.onrender.com/api/firstName', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'  // Required for JSON
