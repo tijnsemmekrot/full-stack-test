31e510ed37206c7b8f6fa8266d3c50823518a828
tijnsemmekrot <tsemmekrot@gmail.com>
2025-06-04 20:41:34 +0200

    removing the result from the return and adding the names from the db

--- backend/main.go
+++ backend/main.go
@@ -112,7 +112,7 @@ func getData(w http.ResponseWriter, r *http.Request) {
 
 	log.Printf("Retrieved documents: %v\n", result)
 	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(result)
+	json.NewEncoder(w).Encode(persons)
 }
 
 // test
